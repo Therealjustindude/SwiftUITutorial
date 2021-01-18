1b1a0a192badea2b1b2a33f02df91a1643034599
therealjustindude <therealjustindude@gmail.com>
2021-01-17 22:23:03 -0600

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Justin Davies on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
